e8491fa4c7ebf4a9d8db1a78308b46ba91b5351e
cyrilf <cyrilf7x@gmail.com>
2019-07-21 13:41:39 +1000

    Updates

---
+++ precache-manifest.41bdf4c32665e5ededeca19bb6c1c711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbaae8eb2be0a0885be829b7b94d9801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e6fbdd6a5dc98cc5d85",
+    "url": "/static/css/main.389fbe3c.chunk.css"
+  },
+  {
+    "revision": "83d1e711fbdb085bcf7e",
+    "url": "/static/js/2.c83062df.chunk.js"
+  },
+  {
+    "revision": "6e6fbdd6a5dc98cc5d85",
+    "url": "/static/js/main.47b8e1a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
